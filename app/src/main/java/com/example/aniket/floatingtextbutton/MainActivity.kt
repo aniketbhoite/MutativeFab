e4ce6299340144dde83b607268626b6da6ff8364
aniketbhoite <aniketbhoite1203@gmail.com>
2017-12-23 22:12:22 +0530

    adding all files

---
+++ app/src/main/java/com/example/aniket/floatingtextbutton/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.aniket.floatingtextbutton
+
+import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.aniket.mutativefloatingactionautton.MutativeFab
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val tv: TextView = findViewById(R.id.card_textView)
+        val constraintLayout: ConstraintLayout = findViewById(R.id.constraintLayout)
+        val button = findViewById<Button>(R.id.button)
+
+        val constraintSet1 = ConstraintSet()
+        constraintSet1.clone(constraintLayout)
+        val constraintSet2 = ConstraintSet()
+        constraintSet2.clone(this, R.layout.activity_main_alt)
+        var changed = false
+
+        val mFab = findViewById<MutativeFab>(R.id.m_fab)
+//        mFab.setFabText("Add household")
+        mFab.apply {
+            setFabText("Add task")
+            setFabTextColor(ContextCompat.getColor(context,R.color.textColor))
+            setFabIcon(R.drawable.ic_add)
+            setFabBackgroundColor(ContextCompat.getColor(context,android.R.color.holo_blue_dark))
+        }
+
+        button.setOnClickListener {
+
+
+
+            mFab.setFabTextVisibility(if (mFab.getFabTextVisibility()) View.GONE else View.VISIBLE)
+
+        }
+
+        val context = baseContext
+
+        mFab.setOnClickListener(View.OnClickListener {
+            Log.d("log","toast ")
+            Toast.makeText(context,"Toast",Toast.LENGTH_LONG).show()
+        })
+
+        mFab.setOnLongClickListener {
+            Toast.makeText(context,"Long Toast",Toast.LENGTH_LONG).show()
+            true
+        }
+
+    }
+
+
+}
